7786bb75baebb8bcddee6c4751d3360a64babf4a
Aubrey Han <aubreyhan@live.com>
2023-12-10 01:58:35 +0000

    20231210

---
+++ project/projectCreate.sh
@@ -0,0 +1,6 @@
+projectName="hy-pingcap-demo001"
+projectID="hy-pingcap001"
+projectFolder="939316359797"
+org="946685524390"
+
+gcloud projects create $projectID --folder=$projectFolder --name=$projectName
